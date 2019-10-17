5d3e76dfd331339af3327dddf5ce577fe3efd160
dericking <dekin100@syr.edu>
2019-10-17 16:11:54 -0400

    Added comments; Added lines to create run summary.

--- run.sh
+++ run.sh
@@ -7,6 +7,9 @@
 ## >>>> SET FDIR TO LOCATION OF YOUR DATA FOLDER
 ## EVENTUALLY WANT TO PUSH THIS TO WEBPAGE AND DATABASE
 
+rm summary.txt
+touch summary.txt
+
 START=$1
 if [[ -z ${2} ]]; then
 END=$1
@@ -26,11 +29,13 @@ FDIR="prexii_sample_data"
 SETF="${FDIR}/mollerrun_${CURR}.set"
 DATF="${FDIR}/moller_data_${CURR}.root"
 
+##CYCLING IF STATEMENT
 if [[ -f $SETF && -f $DATF ]];then
 
 ## Get the helicity pattern, frequency and delay number from the settings file.
 ## We can add more here later.
 
+## Assign numeric value to helicity pattern
 PATT=$(sed -n -e "s#\(Helicity Mode ON/OFF Random/Toggle   HELPATTERNd          : \)##p" $SETF)
 if [ $PATT == "Octet" ]; then
 PATN=8;
@@ -39,16 +44,18 @@ if [ $PATT == "Quartet" ]; then
 PATN=4;
 fi
 
+##Fetch frequency and delay - clean up delay
 FREQ=$(sed -n -e "s#\(Helicity frequency                   HELFREQ              : \)##p" $SETF)
-
-DELN=$( sed -n -e "s#\(Helicity delay                       HELDELAYd            : \)##p" $SETF)
+DELN=$(sed -n -e "s#\(Helicity delay                       HELDELAYd            : \)##p" $SETF)
 DELN=${DELN//[!0-9]/}
 
+##Echo fetched values - can delete or comment out later.
 echo "Data file: ${DATF}"
 echo "H.Pattern: ${PATN}"
 echo " Frequecy: ${FREQ}"
 echo "Delay Num: ${DELN}"
 
+##Run the analysis script.
 root -l <<EOF
 .L eric_asym.C
 eric_asym("${DATF}",${PATN},${DELN},${FREQ})
@@ -56,16 +63,18 @@ EOF
 
 mv analysis_${CURR}.pdf output/
 
+cat "finalStats_"${CURR}".txt" >> summary.txt
+rm "finalStats_"${CURR}".txt"
+
 else
 
 if [[ -f $DATF ]];then
-	ifecho "File  ${DATF}  does not exist!"
+	echo "File  ${DATF}  does not exist!"
 fi
 if [[ -f $SETF ]];then
 	echo "File  ${SETF}  does not exist!"
 fi
 
-
-fi
+fi ##END CYCLEING IF STATEMENT
 
 done
