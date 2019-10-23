b17f5cbe649da8b1430d9fb5917b6ee442321430
dericking <dekin100@syr.edu>
2019-10-23 15:10:34 -0400

    name changes, output pushing, etc.

--- run.sh
+++ run.sh
@@ -4,8 +4,17 @@
 ## FETCHES SETTINGS FILE AND DATA ROOT FILE
 ## TAKES ONE OR TWO ARGUMENTS -- IF ONE ANALYZES SINGLE FILE, IF TWO ANALYZES RANGE
 ## PROBABLY NEED TO INCLUDE SOME CHECKS TO AVOID ERRORS
-## >>>> SET FDIR TO LOCATION OF YOUR DATA FOLDER
+## >>>> SET FILEDIR TO LOCATION OF YOUR DATA FOLDER
 ## EVENTUALLY WANT TO PUSH THIS TO WEBPAGE AND DATABASE
+
+#These will need to be changed.
+#Where to place the analysis results? Where to find the analysis data?
+RSLTDIR="/home/eric/moller_analysis/webpage/analysis"
+[ -d "${RSLTDIR}" ] && echo "Moller online analysis directory exists. Woohoo!!!" || mkdir ${RSLTDIR}
+FILEDIR="prexii_sample_data"
+
+#remove any png from THIS directory.
+rm *.png
 
 rm summary.txt
 touch summary.txt
@@ -19,60 +28,76 @@ fi
 
 echo "Starting analysis for ${START} to ${END}"
 
-#for CURR in {$START..$END}
-for (( CURR=$START; CURR<=$END; CURR++ ))
+#for ANALRUN in {$START..$END}
+for (( ANALRUN=$START; ANALRUN<=$END; ANALRUN++ ))
 do
 
-echo "Analyzing run ${CURR}"
+echo "Analyzing run ${ANALRUN}"
 
-FDIR="prexii_sample_data"
-SETF="${FDIR}/mollerrun_${CURR}.set"
-DATF="${FDIR}/moller_data_${CURR}.root"
+SETFILE="${FILEDIR}/mollerrun_${ANALRUN}.set"
+DATFILE="${FILEDIR}/moller_data_${ANALRUN}.root"
 
 ##CYCLING IF STATEMENT
-if [[ -f $SETF && -f $DATF ]];then
+if [[ -f $SETFILE && -f $DATFILE ]];then
 
-## Get the helicity pattern, frequency and delay number from the settings file.
+## Get the helicity pattern, FREQNCYuency and delay number from the settings file.
 ## We can add more here later.
 
 ## Assign numeric value to helicity pattern
-PATT=$(sed -n -e "s#\(Helicity Mode ON/OFF Random/Toggle   HELPATTERNd          : \)##p" $SETF)
-if [ $PATT == "Octet" ]; then
-PATN=8;
+PATTERN=$(sed -n -e "s#\(Helicity Mode ON/OFF Random/Toggle   HELPATTERNd          : \)##p" $SETFILE)
+if [ $PATTERN == "Octet" ]; then
+PATTERN=8;
 fi
-if [ $PATT == "Quartet" ]; then
-PATN=4;
+if [ $PATTERN == "Quartet" ]; then
+PATTERN=4;
 fi
 
-##Fetch frequency and delay - clean up delay
-FREQ=$(sed -n -e "s#\(Helicity frequency                   HELFREQ              : \)##p" $SETF)
-DELN=$(sed -n -e "s#\(Helicity delay                       HELDELAYd            : \)##p" $SETF)
-DELN=${DELN//[!0-9]/}
+##Fetch FREQNCYuency and delay - clean up delay
+FREQNCY=$(sed -n -e "s#\(Helicity frequency                   HELFREQ              : \)##p" $SETFILE)
+HELDLAY=$(sed -n -e "s#\(Helicity delay                       HELDELAYd            : \)##p" $SETFILE)
+HELDLAY=${HELDLAY//[!0-9]/}
+RUNDATE=$(sed -n -e "s#\(Date       : \)##p" $SETFILE)
+read dayname month datnum clock zone year <<< ${RUNDATE}
+#RUNDATE=$(date --date="$(printf "${RUNDATE}")" +%Y-%m-%d_%H-%M-%S)
+RUNDATE=$(date --date="$(printf "${RUNDATE}")" +%Y-%m-%d)
+RUNTIME=$(date --date="$(printf "${RUNDATE}")" +%H-%M-%S)
 
 ##Echo fetched values - can delete or comment out later.
-echo "Data file: ${DATF}"
-echo "H.Pattern: ${PATN}"
-echo " Frequecy: ${FREQ}"
-echo "Delay Num: ${DELN}"
+echo "Data file: ${DATFILE}"
+echo "H.Pattern: ${PATTERN}"
+echo "Frequency: ${FREQNCY}"
+echo "Delay Num: ${HELDLAY}"
+echo "Date/Time: ${RUNDATE}"
 
 ##Run the analysis script.
 root -l <<EOF
 .L eric_asym.C
-eric_asym("${DATF}",${PATN},${DELN},${FREQ})
+eric_asym("${DATFILE}",${PATTERN},${HELDLAY},${FREQNCY})
 EOF
 
-mv analysis_${CURR}.pdf output/
+[ ! -d "${RSLTDIR}/files" ]            && mkdir ${RSLTDIR}/files
+[ ! -d "${RSLTDIR}/files/${ANALRUN}" ] && mkdir ${RSLTDIR}/files/run_${ANALRUN}
 
-cat "finalStats_"${CURR}".txt" >> summary.txt
-rm "finalStats_"${CURR}".txt"
+mv  analysis_${ANALRUN}.pdf    ${RSLTDIR}/files/run_${ANALRUN}/
+cp  final_stats_${ANALRUN}.txt  ${RSLTDIR}/files/run_${ANALRUN}/
+mv  errors_${ANALRUN}.txt       ${RSLTDIR}/files/run_${ANALRUN}/
+mv  *.png                      ${RSLTDIR}/files/run_${ANALRUN}/
+
+[ ! -d "${RSLTDIR}/date" ]                       && mkdir ${RSLTDIR}/date
+[ ! -d "${RSLTDIR}/date/${RUNDATE}" ]            && mkdir ${RSLTDIR}/date/${RUNDATE}
+
+ln    -s  ${RSLTDIR}/files/run_${ANALRUN}  ${RSLTDIR}/date/${RUNDATE}/
+
+cat "final_stats_${ANALRUN}.txt" >> summary.txt
+rm "final_stats_${ANALRUN}.txt"
 
 else
 
-if [[ -f $DATF ]];then
-	echo "File  ${DATF}  does not exist!"
+if [[ -f $DATFILE ]];then
+	echo "File  ${DATFILE}  does not exist!"
 fi
-if [[ -f $SETF ]];then
-	echo "File  ${SETF}  does not exist!"
+if [[ -f $SETFILE ]];then
+	echo "File  ${SETFILE}  does not exist!"
 fi
 
 fi ##END CYCLEING IF STATEMENT
