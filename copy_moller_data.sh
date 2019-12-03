cc04403820510e33f0be7b03abaa616c7b51daea
David King <ericking@ifarm1402.jlab.org>
2019-12-03 16:35:39 -0500

    added copy_moller_data.sh & run_analysis.sh to feature branch

---
+++ copy_moller_data.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+FILEDIR=$1
+
+STARTRUN=$2
+if [[ -z ${3} ]]; then
+ENDRUN=$2
+else
+ENDRUN=${3}
+fi
+
+DATADIR="/adaq1/data1/moller"
+SETSDIR="/adaqfs/home/moller/daq/coda2/RunInfo"
+
+for i in $(seq $STARTRUN $ENDRUN);do
+    FILE2=${DATADIR}/moller_data_${i}.nt
+    cp $FILE2 ./$FILEDIR/
+    FILE3=${SETSDIR}/mollerrun_${i}.set
+    cp $FILE3 ./$FILEDIR/
+done
